8f2d2149b8190e951cdd236a1b75916ac5885edc
Kelly Small <xcxkelly98@gmail.com>
2019-11-02 04:36:05 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
 def addition(num1, num2)
-
+describe"#addition" do 
+  it "adds two numbers together" do
+    expect(add (3,3)).to eq(6)
 end
 
 def subtraction(num1, num2)
-
+  describe"#subtraction" do
+    it "subtracts two numbers from each other" do
+      expect(subtract (10,3)).to eq(7)
 end
 
 def division(num1, num2)
-
+  describe "#division" do
+    it "divides one number from the other"
+      expect(divide (12,3)).to eq(4)
 end
 
 def multiplication(num1, num2)
+describe "#multiplication" do
+  it "multiplies two numbers together"
+     expect(multiply (5,4)).to eq(20)
 
 end
 
 def modulo(num1, num2)
-
+describe "#modulo" do 
+  it "#returns the remainder of two numbers"
+    expect(modulo (35,5)).to eq(4)
 end
 
 def square_root(num)
-
+describe "square_root" do
+  it "returns the square root of a number"
+   expect(square_root(81)).to eq(9)
 end
